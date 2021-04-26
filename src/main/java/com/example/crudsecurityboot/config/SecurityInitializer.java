e85d17bb298ce059f7aaa20043255e0ee214092c
simdrednovnikolay <61241753+simdrednovnikolay@users.noreply.github.com>
2021-04-26 20:10:59 +0400

    Add files via upload

---
+++ src/main/java/com/example/crudsecurityboot/config/SecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.example.crudsecurityboot.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
